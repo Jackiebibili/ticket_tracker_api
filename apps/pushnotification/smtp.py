3f84aba9d63d29cc05de98a943cc69b096907b72
Frank Gao <frank.qixiang.gao@gmail.com>
2022-10-20 16:59:25 -0500

    wrap into function

--- apps/pushnotification/smtp.py
+++ apps/pushnotification/smtp.py
@@ -4,29 +4,31 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 
-load_dotenv(find_dotenv())
 
-port = 465  # For starttls
-smtp_server = "smtp.gmail.com"
-sender_email = "noreply.ticketmasterbestseat@gmail.com"
-receiver_email = ["frank.qixiang.gao@gmail.com"]
-password = "?????????" # The password is stored in local
-subject = "Email from ticketmaster Best Seat App"
+def send_email(to_email, message):
 
-message = """\
-Subject: Hi there
+    load_dotenv(find_dotenv())
 
-This message is sent from Python to give best seat info."""
+    port = 465  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = "noreply.ticketmasterbestseat@gmail.com"
+    receiver_email = to_email
+    password = "?????????" # The password is stored in local
+    subject = "Message from Ticketmaster Ticket-Tracker"
 
-em = EmailMessage()
-em['From'] = sender_email
-em['To'] = receiver_email
-em['subject'] = subject
+    message = """\
+    Subject: Hi there
+    This message is sent from Python to give best seat info."""
 
-em.set_content(message)
+    em = EmailMessage()
+    em['From'] = sender_email
+    em['To'] = receiver_email
+    em['subject'] = subject
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, em.as_string())
+    em.set_content(message)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, em.as_string())
 
