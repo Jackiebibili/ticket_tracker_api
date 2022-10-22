bff25ce2ea8ef3958b912e993de90ca3e4fed68a
Frank Gao <frank.qixiang.gao@gmail.com>
2022-10-21 19:33:07 -0500

    refactor

--- apps/pushnotification/constants.py
+++ apps/pushnotification/constants.py
@@ -2,3 +2,5 @@ port = 465  # For starttls
 smtp_server = "smtp.gmail.com"
 sender_email = "noreply.ticketmasterbestseat@gmail.com"
 subject = "Message from Ticketmaster Ticket-Tracker"
+app_password = "XXXXXXXXXXXX" # The password is stored in local
+receiver_email = ["noreply.ticketmasterbest@gmail.com"]
--- apps/pushnotification/smtp.py
+++ apps/pushnotification/smtp.py
@@ -1,18 +1,31 @@
 import smtplib, ssl
 from email.message import EmailMessage
+import os
 from dotenv import load_dotenv, find_dotenv
-from constants import * 
+from constants import *
 
-def send_email(to_email, message):
+load_dotenv(find_dotenv())
 
-    load_dotenv(find_dotenv())
+receiver_email = receiver_email
+password = app_password # The password is stored in local
 
-    receiver_email = to_email
-    password = "?????????" # The password is stored in local
-
-    message = """\
+message = """\
     Subject: Hi there
     This message is sent from Python to give best seat info."""
+
+def init_server():
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context)
+    return server
+
+def server_login(server, password):
+    email = sender_email    
+    server.login(email, password)
+
+def server_send_email(server, receiver_email, message):
+    receiver_email = receiver_email
+    sender_email = sender_email
+    subject = "Email from ticketmaster Best Seat App"
 
     em = EmailMessage()
     em['From'] = sender_email
@@ -20,9 +33,9 @@ def send_email(to_email, message):
     em['subject'] = subject
 
     em.set_content(message)
+    server.sendmail(sender_email, receiver_email, em.as_string())
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, em.as_string())
 
+server = init_server()
+server_login(server, password)  
+server_send_email(server, receiver_email, message)
