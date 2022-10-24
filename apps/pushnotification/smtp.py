8648f488355778a89ee95e5368e133d7e33e2420
Frank Gao <frank.qixiang.gao@gmail.com>
2022-10-24 11:41:42 -0500

    add send_email

--- apps/pushnotification/smtp.py
+++ apps/pushnotification/smtp.py
@@ -1,17 +1,8 @@
 import smtplib, ssl
 from email.message import EmailMessage
-import os
-from dotenv import load_dotenv, find_dotenv
 from constants import *
 
-load_dotenv(find_dotenv())
-
-receiver_email = receiver_email
 password = app_password # The password is stored in local
-
-message = """\
-    Subject: Hi there
-    This message is sent from Python to give best seat info."""
 
 def init_server():
     context = ssl.create_default_context()
@@ -35,7 +26,10 @@ def server_send_email(server, receiver_email, message):
     em.set_content(message)
     server.sendmail(sender_email, receiver_email, em.as_string())
 
+def send_email(receiver_email, message):
+    server = init_server()
+    server_login(server, password)
+    server_send_email(server, receiver_email, message)
 
-server = init_server()
-server_login(server, password)  
-server_send_email(server, receiver_email, message)
+
+send_email(["Frank.Qixiang.Gao@gmail.com"], password)
